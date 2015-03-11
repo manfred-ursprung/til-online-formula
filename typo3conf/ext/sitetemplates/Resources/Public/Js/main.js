d78608bd982906c11070a788e628aecc657906f6
Manfred <manfred@manfred-ursprung.de>
2015-03-11 14:43:21 +0100

    Login Formular : Template gestylt, Login Menüpunkt, Login Form als Modal-Dialog-Box geht noch nicht. Soll erstmal über Link zu einer Seite erfolgen.

--- typo3conf/ext/sitetemplates/Resources/Public/Js/main.js
+++ typo3conf/ext/sitetemplates/Resources/Public/Js/main.js
@@ -1,3 +1,22 @@
 /**
  * Created by manfred on 27.02.15.
  */
+
+/* Toggle Login Formular */
+$(document).ready(function(){
+    $('[data-toggle="login"]').on('click', function(){
+        $(this).modal('toggle');
+    })
+
+});
+/*
+("click.bs.modal.data-api", '[data-toggle="modal"]', function (c) {
+    var d = a(this), e = d.attr("href"), f = a(d.attr("data-target") || e && e.replace(/.*(?=#[^\s]+$)/, "")), g = f.data("bs.modal") ? "toggle" : a.extend({remote: !/#/.test(e) && e}, f.data(), d.data());
+    d.is("a") && c.preventDefault(), f.one("show.bs.modal", function (a) {
+        a.isDefaultPrevented() || f.one("hidden.bs.modal", function () {
+            d.is(":visible") && d.trigger("focus")
+        })
+    }), b.call(f, g, this)
+})
+}
+*/
